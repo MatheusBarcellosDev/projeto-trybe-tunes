7df22bcf5c3bb650c9b5185d82afeb931f5c0688
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2022-01-13 23:59:40 -0300

    Requisito 8

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,21 +1,63 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from '../pages/Loading';
 
 class MusicCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      favorite: false,
+    };
+  }
+
+  handleClick = ({ target: { checked } }) => {
+    const { data: { trackId } } = this.props;
+    this.setState({
+      loading: true,
+    });
+    addSong(trackId, checked)
+      .then(() => {
+        this.setState({
+          loading: false,
+          favorite: checked,
+        });
+      });
+  };
+
   render() {
     const { data } = this.props;
+    const { favorite, loading } = this.state;
 
     return (
       <div>
-        <p>{ data.trackName }</p>
+        {loading ? <Loading /> : (
+          <>
+            <p>{ data.trackName }</p>
 
-        <audio data-testid="audio-component" src={ data.previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          {' '}
-          <code>audio</code>
-          .
-        </audio>
+            <audio data-testid="audio-component" src={ data.previewUrl } controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              {' '}
+              <code>audio</code>
+              .
+            </audio>
+            <label htmlFor="favorite">
+              Favorita
+              <input
+                id="favorite"
+                data-testid={ `checkbox-music-${data.trackId}` }
+                onClick={ this.handleClick }
+                checked={ favorite }
+                type="checkbox"
+              />
+            </label>
+          </>
+
+        )}
+
       </div>
     );
   }
